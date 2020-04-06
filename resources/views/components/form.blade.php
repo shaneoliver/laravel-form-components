13d871b2927cc6c9e4409e0a44c3fc119aed6048
Shane Oliver <olivershan@gmail.com>
2020-04-07 09:14:34 +1000

    Add buttons slot to form

--- resources/views/components/form.blade.php
+++ resources/views/components/form.blade.php
@@ -4,6 +4,12 @@
         {{ $slot }}
     </div>
     <div class="form-row">
-        <button class="btn btn-primary btn-lg">{{ $button ?? 'Submit' }}</button>
+        <div class="col-12">
+            @if ($buttons ?? '')
+                {{ $buttons }}
+            @else
+                <button class="btn btn-primary btn-lg">{{ $button ?? 'Submit' }}</button>
+            @endif
+        </div>
     </div>
 </form>
